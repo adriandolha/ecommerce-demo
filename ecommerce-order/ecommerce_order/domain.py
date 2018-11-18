2c37fca73bee6fd1531743579937c2a068e95c7a
adolha <adrian.dolha@endava.com>
2018-11-18 12:28:04 +0200

    Order API - add DynamoDB lambda trigger

--- ecommerce-order/ecommerce_order/domain.py
+++ ecommerce-order/ecommerce_order/domain.py
@@ -1,12 +1,12 @@
 class Order:
-    def __init__(self, order_id, items, user_id="1", status="CREATED"):
+    def __init__(self, order_id, items, user_id="1", status="CREATED", total=None):
         self.order_id = order_id
         self.status = status
         self.user_id = user_id
         self.items = items
         for item in items:
             item['total'] = int(item['price']) * int(item['count'])
-        self.total = sum([int(item['price']) * int(item['count']) for item in items])
+        self.total = total or sum([int(item['price']) * int(item['count']) for item in items])
 
     def to_json(self):
         return {
